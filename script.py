52944b27cc668d638928a288c6e79b9ac06bd906
Logesh <116418351+LPSMOVIES@users.noreply.github.com>
2022-10-26 09:10:38 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,66 @@
+class Script(object):
+
+    START_MSG = """<b>Hi 👋 {},
+
+I'm an advanced filter bot with many capabilities!
+
+<a href="https://t.me/tamilnew_movieschat">1☞𝙅𝙤𝙞𝙣 𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝘾𝙝𝙖𝙣𝙣𝙚𝙡</a>
+
+MADE BY @Logeshps_bot2
+
+See <i>/help</i> for commands and more details.</b>
+"""
+
+
+    HELP_MSG = """
+<i>Add me as admin in your group and start filtering :)\nFor More Details CONTACT HERE @Logeshps_bot2</i>
+
+
+<b>Basic Commands;</b>
+
+/start - Check if I'm alive!
+/help - Command help
+/about - Something about me!
+
+
+<b>Filter Commands;</b>
+
+<code>/add name reply</code>  -  Add filter for name
+
+<code>/del name</code>  -  Delete filter
+
+<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
+
+<code>/viewfilters</code>  -  List all filters in chat
+
+
+<b>Connection Commands;</b>
+
+<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
+<code>/connect</code> in groups.
+
+<code>/connections</code>  -  Manage your connections.
+
+
+<b>Extras;</b>
+
+/status  -  Shows current status of your bot (Auth User Only)
+
+/id  -  Shows ID information
+
+<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+
+
+<b>© @Logeshps_bot2</b>
+"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : LPS Filter Bot</b>
+
+⭕️<b>Creater :</b> @Logeshps_bot2 
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+"""
